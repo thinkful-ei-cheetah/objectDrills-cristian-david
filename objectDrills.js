2dfeef83084e537a910eedb78f33d54f80942d54
Cristian Camarero <cristian.j.camarero@gmail.com>
2019-03-27 08:44:19 -0700

    finished object drills 1

--- objectDrills.js
+++ objectDrills.js
@@ -12,7 +12,34 @@ function createMyObject() {
 function updateObject(obj) {
     obj.foo= 'foo';
     obj.bar= 'bar';
-    obj.bizz= 'biz';
+    obj.bizz= 'bizz';
     obj.bang= 'bang';
     return obj;
 }
+
+function personMaker() {
+    var person = {
+      firstName: 'Paul',
+      lastName: 'Jones',
+      // replace `null` with a function that uses self reference to return
+      // full name
+      fullName: function() {
+          return `${this.firstName}` + ' ' + `${this.lastName}`;
+      }
+    };
+    return person;
+  }
+
+const sampleObj = {
+foo: 'foo',
+bar: 'bar',
+bizz: 'bizz',
+bang: 'bang',
+};
+
+function keyDeleter(obj) {
+    delete sampleObj.foo;
+    delete sampleObj.bar;
+    return sampleObj;
+}
+
