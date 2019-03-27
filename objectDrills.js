a0543b746b30f6840e62bb2bf2ef9dc87e35aee7
David Nordeen <davidnordeen@gmail.com>
2019-03-27 12:03:03 -0700

    object drills 2 finished

--- objectDrills.js
+++ objectDrills.js
@@ -10,31 +10,31 @@ function createMyObject() {
 }
 
 function updateObject(obj) {
-    obj.foo= 'foo';
-    obj.bar= 'bar';
-    obj.bizz= 'bizz';
-    obj.bang= 'bang';
+    obj.foo = 'foo';
+    obj.bar = 'bar';
+    obj.bizz = 'bizz';
+    obj.bang = 'bang';
     return obj;
 }
 
 function personMaker() {
     var person = {
-      firstName: 'Paul',
-      lastName: 'Jones',
-      // replace `null` with a function that uses self reference to return
-      // full name
-      fullName: function() {
-          return `${this.firstName}` + ' ' + `${this.lastName}`;
-      }
+        firstName: 'Paul',
+        lastName: 'Jones',
+        // replace `null` with a function that uses self reference to return
+        // full name
+        fullName: function () {
+            return `${this.firstName}` + ' ' + `${this.lastName}`;
+        }
     };
     return person;
-  }
+}
 
 const sampleObj = {
-foo: 'foo',
-bar: 'bar',
-bizz: 'bizz',
-bang: 'bang',
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
 };
 
 function keyDeleter(obj) {
@@ -52,12 +52,34 @@ function makeStudentsReport(data) {
 }
 
 function enrollInSummerSchool(students) {
-    return updatedArray = students.map(students =>{ 
-      return { 
-        name: students.name, 
-        status: 'In Summer school', 
-        course: students.course, 
-        }; 
-        }); 
+    return updatedArray = students.map(students => {
+        return {
+            name: students.name,
+            status: 'In Summer school',
+            course: students.course,
+        };
+    });
+}
+
+
+function findById(items, idNum) {
+    for (let i = 0; i < items.length; i++)
+        if (idNum === items[i].id) {
+            return items[i];
         }
+}
+
+function validateKeys(object, expectedKeys) {
+    if (Object.keys(object).length !== expectedKeys.length) {
+        return false
+    }
+    for (let i = 0; i < expectedKeys.length; i++)
+        if (Object.keys(object)[i] !== expectedKeys[i]) {
+            return false
+        }
+        else {
+            return true
+        }
+
+}
 
